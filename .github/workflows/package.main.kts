7a7f40d363973db0ae58c54fa890f1ceeddf2e2f
Nyayurin <nyayurn@outlook.com>
2025-12-16 19:46:29 +0800

    Rewrite workflow with kotlin

---
+++ .github/workflows/package.main.kts
@@ -0,0 +1,159 @@
+#!/usr/bin/env kotlin
+
+@file:Repository("https://repo.maven.apache.org/maven2/")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:3.6.0")
+@file:Repository("https://bindings.krzeminski.it")
+@file:DependsOn("actions:checkout:v4")
+@file:DependsOn("actions:setup-java:v4")
+@file:DependsOn("actions:upload-artifact:v4")
+@file:DependsOn("gradle:actions__setup-gradle:v4")
+
+import io.github.typesafegithub.workflows.actions.actions.Checkout
+import io.github.typesafegithub.workflows.actions.actions.SetupJava
+import io.github.typesafegithub.workflows.actions.actions.UploadArtifact
+import io.github.typesafegithub.workflows.actions.gradle.ActionsSetupGradle
+import io.github.typesafegithub.workflows.domain.JobOutputs
+import io.github.typesafegithub.workflows.domain.Mode
+import io.github.typesafegithub.workflows.domain.Permission
+import io.github.typesafegithub.workflows.domain.RunnerType
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.domain.triggers.Release
+import io.github.typesafegithub.workflows.dsl.JobBuilder
+import io.github.typesafegithub.workflows.dsl.WorkflowBuilder
+import io.github.typesafegithub.workflows.dsl.workflow
+import java.io.File
+
+val content: WorkflowBuilder.() -> Unit = {
+	publish(
+		runsOn = RunnerType.WindowsLatest,
+		system = "windows",
+		arch = "amd64",
+		publishes = listOf(
+			"uberjar" to "UberJarForCurrentOS" to "jars",
+			"portable" to "AppImage" to "binaries/main/app",
+			"installer-msi" to "Msi" to "binaries/main/msi",
+			"installer-exe" to "Exe" to "binaries/main/exe",
+		),
+	)
+	publish(
+		runsOn = RunnerType.Custom("windows-11-arm"),
+		system = "windows",
+		arch = "aarch64",
+		publishes = listOf(
+			"uberjar" to "UberJarForCurrentOS" to "jars",
+			"portable" to "AppImage" to "binaries/main/app",
+			"installer-msi" to "Msi" to "binaries/main/msi",
+			"installer-exe" to "Exe" to "binaries/main/exe",
+		),
+	)
+	publish(
+		runsOn = RunnerType.UbuntuLatest,
+		system = "linux",
+		arch = "x64",
+		publishes = listOf(
+			"uberjar" to "UberJarForCurrentOS" to "jars",
+			"portable" to "AppImage" to "binaries/main/app",
+			"installer-deb" to "Deb" to "binaries/main/deb",
+			"installer-rpm" to "Rpm" to "binaries/main/rpm",
+		),
+	)
+	publish(
+		runsOn = RunnerType.Custom("ubuntu-22.04-arm"),
+		system = "linux",
+		arch = "aarch64",
+		publishes = listOf(
+			"uberjar" to "UberJarForCurrentOS" to "jars",
+			"portable" to "AppImage" to "binaries/main/app",
+			"installer-deb" to "Deb" to "binaries/main/deb",
+			"installer-rpm" to "Rpm" to "binaries/main/rpm",
+		),
+	)
+	publish(
+		runsOn = RunnerType.MacOSLatest,
+		system = "macos",
+		arch = "aarch64",
+		publishes = listOf(
+			"uberjar" to "UberJarForCurrentOS" to "jars",
+			"installer-dmg" to "Dmg" to "binaries/main/dmg",
+		),
+	)
+}
+
+workflow(
+	name = "Package on Push",
+	on = listOf(
+		Push(
+			branches = listOf("master"),
+			pathsIgnore = listOf("README.md")
+		),
+	),
+	sourceFile = File("package_on_push.main.kts"),
+	block = content,
+)
+
+workflow(
+	name = "Package on Release",
+	on = listOf(
+		Release(
+			types = listOf("created"),
+		),
+	),
+	sourceFile = File("package_on_release.main.kts"),
+	block = content,
+)
+
+infix fun <T1, T2, T3> Pair<T1, T2>.to(third: T3): Triple<T1, T2, T3> = Triple(this.first, this.second, third)
+
+fun WorkflowBuilder.publish(runsOn: RunnerType, system: String, arch: String, publishes: List<Triple<String, String, String>>) {
+	job(
+		id = "package-$system-$arch",
+		runsOn = runsOn,
+		permissions = mapOf(
+			Permission.Contents to Mode.Read,
+			Permission.Packages to Mode.Write,
+		)
+	) {
+		uses(
+			action = Checkout(),
+		)
+		uses(
+			action = SetupJava(
+				javaVersion = "21",
+				distribution = SetupJava.Distribution.Zulu,
+			),
+		)
+		uses(
+			action = ActionsSetupGradle(),
+		)
+		run(
+			command = """
+				chmod +x ./gradlew
+				./gradlew :generateComposeResClass
+				./gradlew :generateResourceAccessorsForComposeMain
+				./gradlew :generateExpectResourceCollectorsForCommonMain
+				./gradlew :generateActualResourceCollectorsForComposeJvmMain
+				./gradlew :assembleComposeJvmMainResources
+			""".trimIndent(),
+		)
+		publishes.forEach { publish ->
+			publish(
+				name = "$system-${publish.first}-$arch",
+				task = publish.second,
+				path = publish.third,
+			)
+		}
+	}
+}
+
+fun JobBuilder<JobOutputs.EMPTY>.publish(name: String, task: String, path: String) {
+	run(
+		name = "package $name",
+		command = "./gradlew :packageRelease$task",
+	)
+	uses(
+		action = UploadArtifact(
+			name = "MCL-$name",
+			path = listOf("build/compose/$path/*"),
+		),
+	)
+}
